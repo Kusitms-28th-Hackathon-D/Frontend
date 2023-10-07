f16b33747785b92cef783b7b7857ab5c7399f6c0
SuJin <kimsujin001127@gmail.com>
2023-10-08 03:59:46 +0900

    feat: 질문추가 컴포넌트 분리

--- src/components/question/MakeQuestion.js
+++ src/components/question/MakeQuestion.js
@@ -1,26 +1,20 @@
 import SmallLogo from '../images/question/SmallLogo.svg';
 import RightArrow from '../images/question/RightArrow.svg';
 import CircleNumber from './CircleNumber';
-import Delete from '../images/question/Delete.svg';
 import Plus from '../images/question/Plus.svg';
 import Tag from '../signup/Tag';
 import { useState } from 'react';
-import OptionInput from './OptionInput';
+import AddQuestion from './AddQuestion';
 
 const MakeQuestion = () => {
-  const [inputs, setInputs] = useState([]);
-  const [newInput, setNewInput] = useState('');
+  const [addQuestions, setAddQuestions] = useState([]);
+  const [newQuestion, setNewQuestion] = useState('');
 
-  const addInput = () => {
-    setInputs([...inputs, newInput]);
-    setNewInput('');
+  const hanldeAddQuestion = () => {
+    setAddQuestions([...addQuestions, newQuestion]);
+    setNewQuestion('');
   };
 
-  const removeInput = (index) => {
-    const updatedInputs = [...inputs];
-    updatedInputs.splice(index, 1);
-    setInputs(updatedInputs);
-  };
   return (
     <main className="flex flex-col justify-center items-center text-center mx-auto">
       <div className="flex flex-row mt-20">
@@ -101,53 +95,13 @@ const MakeQuestion = () => {
       </div>
       <div className="mt-10 w-[750px] flex flex-col items-start">
         <div className="text-[32px] font-medium">추가 질문</div>
-        <div className="flex flex-row">
-          <div className="flex flex-row mt-5">
-            <div className="flex w-[82px] mr-3">
-              <div className="flex items-center">
-                <CircleNumber number={1} />
-              </div>
-              <div className="flex items-center">
-                <img className="flex items-center mr-3 h-[16px]" src={RightArrow} />
-              </div>
-            </div>
-            <div className="flex items-center font-medium text-[24px]">추가로 물어보고 싶은 질문은 무엇인가요?</div>
-          </div>
+        <AddQuestion />
+        <div
+          onClick={hanldeAddQuestion}
+          className="bg-[#D7F1E6] w-[642px] text-[20px] mt-10 rounded-full flex items-center justify-center"
+        >
+          +
         </div>
-        <div className="ml-[93px]">
-          <input
-            className="mt-3 w-[542px] pb-[3px] text-[24px] border-b-2 border-[#D9D9D9] focus:outline-none"
-            type="text"
-            placeholder="질문의 제목을 작성해주세요."
-          />
-        </div>
-        <div className="flex flex-row mt-5">
-          <div className="flex w-[97px] mr-3">
-            <div className="flex items-center">
-              <div className="rounded-full w-[58px] h-[44px] bg-[#E9F5F0] flex justify-center items-center text-center mr-3">
-                {'1-2'}
-              </div>
-            </div>
-            <div className="flex items-center">
-              <img className="flex items-center mr-3 h-[16px]" src={RightArrow} />
-            </div>
-          </div>
-          <div className="flex items-center font-medium text-[24px]">질문의 옵션을 작성해주세요!</div>
-          <div
-            onClick={addInput}
-            className="flex items-center justify-center w-[44px] h-[44px] bg-[#F2F2F2] rounded-full ml-[10px]"
-          >
-            <img src={Plus} />
-          </div>
-        </div>
-        <div className="ml-[93px] mt-5 grid grid-cols-3 gap-12">
-          <OptionInput />
-          {inputs.map((input, index) => (
-            <OptionInput removeInput={removeInput} index={index} />
-          ))}
-        </div>
-        <div className="bg-[#D7F1E6] w-[642px] text-[20px] mt-10 rounded-full flex items-center justify-center">+</div>
-        <div></div>
       </div>
       <div className="my-[50px] rounded-[9px] bg-[#55B68F] w-[240px] h-[67px] flex items-center justify-center text-[white]">
         질문 폼 작성 완료하기
