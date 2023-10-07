efef9b5ebe2714eb2367dffc52d52bb6b0451381
SuJin <kimsujin001127@gmail.com>
2023-10-08 06:42:27 +0900

    feat: navbar 추가

--- src/components/signup/Ranking.js
+++ src/components/signup/Ranking.js
@@ -5,6 +5,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { newUserState } from '../../states/signup';
 import axios from 'axios';
+import Navbar from '../base/Navbar';
 
 const Ranking = () => {
   const [selectedTagIndex, setSelectedTagIndex] = useState([[-1], [-1], [-1]]); // 선택된 Tag의 인덱스, 초기값 -1로 설정
@@ -47,35 +48,38 @@ const Ranking = () => {
   };
 
   return (
-    <main className="flex flex-col justify-center items-center text-center mx-auto">
-      <h2 className="text-[33.75px] font-semibold mt-20 text-[#55B68F]">회원가입</h2>
-      <div className="mt-12">
-        {[1, 2, 3].map((ranking) => (
-          <div key={ranking} className="flex mt-7">
-            <Number number={ranking} />
-            {Tags.map((tag, index) => (
-              <Tag
-                key={index}
-                text={tag}
-                isSelected={selectedTagIndex[ranking - 1][0] === index}
-                onClick={() => handleTagClick(ranking - 1, index)}
-              />
-            ))}
+    <>
+      <Navbar />
+      <main className="flex flex-col justify-center items-center text-center mx-auto">
+        <h2 className="text-[33.75px] font-semibold mt-20 text-[#55B68F]">회원가입</h2>
+        <div className="mt-12">
+          {[1, 2, 3].map((ranking) => (
+            <div key={ranking} className="flex mt-7">
+              <Number number={ranking} />
+              {Tags.map((tag, index) => (
+                <Tag
+                  key={index}
+                  text={tag}
+                  isSelected={selectedTagIndex[ranking - 1][0] === index}
+                  onClick={() => handleTagClick(ranking - 1, index)}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="mt-12 flex flex-rol">
+          <div className="w-[197px] h-[48px] flex justify-center text-center items-center rounded-[50px] text-[16px] border border-solid border-[#55B68F] border-1 text-[#55B68F] mr-5">
+            <Link to="/signup/2">이전</Link>
           </div>
-        ))}
-      </div>
-      <div className="mt-12 flex flex-rol">
-        <div className="w-[197px] h-[48px] flex justify-center text-center items-center rounded-[50px] text-[16px] border border-solid border-[#55B68F] border-1 text-[#55B68F] mr-5">
-          <Link to="/signup/2">이전</Link>
+          <div
+            onClick={handleSubmit}
+            className="w-[197px] h-[48px] flex justify-center text-center items-center rounded-[50px] text-[16px] bg-[#55B68F] text-[white]"
+          >
+            <Link to="/signup/3">가입하기</Link>
+          </div>
         </div>
-        <div
-          onClick={handleSubmit}
-          className="w-[197px] h-[48px] flex justify-center text-center items-center rounded-[50px] text-[16px] bg-[#55B68F] text-[white]"
-        >
-          <Link to="/signup/3">가입하기</Link>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
